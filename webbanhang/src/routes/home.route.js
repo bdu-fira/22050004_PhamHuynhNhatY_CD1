425257d99ee087fe87b56f8e9a84205ca4dabb8f
Phạm Huỳnh Nhật Ý <22050004@student.bdu.edu.vn>
2025-05-21 13:11:30 +0700

    Delete webbanhang directory

--- webbanhang/src/routes/home.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const homeController = require('../controllers/home.controller');
-
-// Route cho trang chủ
-router.get('/', homeController.getHomePage);
-
-module.exports = router;
